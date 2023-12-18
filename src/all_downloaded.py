32996ec78157a66c24fbc6a377c85e4c7280c64a
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2023-12-18 14:12:04 +0200

    Add n_urls parameter

--- src/all_downloaded.py
+++ src/all_downloaded.py
@@ -3,13 +3,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", type=str, help="path to parent dir of files")
-args = parser.parse_args()
+parser.add_argument("--n_urls", type=int, help="number of urls")
 
-def all_downloaded(path):
+
+def all_downloaded(path,n_urls):
     files = gather_files(path)
-    res = 'true' if len(files)==50000 else 'false'
+    res = 'true' if len(files)==n_urls else 'false'
     return res
 
-
 if __name__ == '__main__':
-    print(all_downloaded(args.path))
+    args = parser.parse_args()
+    print(all_downloaded(args.path,args.n_urls))
