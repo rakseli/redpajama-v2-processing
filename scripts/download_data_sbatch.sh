8cd5aab4877a3cb4a236b3bec5ed46fc29e68be1
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2023-12-20 16:44:32 +0200

    Downloads all data types of single crawl in slurm script

--- scripts/download_data_sbatch.sh
+++ scripts/download_data_sbatch.sh
@@ -1,20 +1,46 @@
 #!/bin/bash
-#SBATCH --job-name=download_data
-#SBATCH --output=./logs/download_data_%j.output # Name of stdout output file
-#SBATCH --error=./logs/download_data_%j.erros  # Name of stderr error file
+#SBATCH --job-name=download_one_crawl_data
+#SBATCH --output=../logs/download_one_crawl_data_%j.output # Name of stdout output file
+#SBATCH --error=../logs/download_one_crawl_data_%j.erros  # Name of stderr error file
 #SBATCH --account=project_462000086
-#SBATCH --time=33:00:00 #change larger for real run
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=128
-#SBATCH --mem-per-cpu=1750
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=1500
 #SBATCH --partition=small 
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 module purge
 module load LUMI/22.12 
 module load parallel/20230322
 module load wget/1.21.3-cpeAOCC-22.12
+module load cray-python
 
-cd /scratch/project_462000086/data/redpajama-v2/texts-2023-14
-cat /scratch/project_462000086/data/redpajama-v2/2023-14-head-middle-urls.txt | parallel -j 8 wget -q -x --cut-dirs 2 {}
+# $1 test run true or else
+# $2 crawl id
+# $3 data_type
+srun \
+        bash download_crawl.sh \
+        f \
+        2023-14 \
+        duplicates
+
+srun \
+        bash download_crawl.sh \
+        f \
+        2023-14 \
+        minhash
+
+srun \
+        bash download_crawl.sh \
+        f \
+        2023-14 \
+        quality_signals
+
+ 
+srun \
+        bash download_crawl.sh \
+        f \
+        2023-14 \
+        texts
+
