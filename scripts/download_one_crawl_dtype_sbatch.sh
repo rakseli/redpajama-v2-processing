68d68efb35761df78ebba7695d42a8e016e5ffe4
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2023-12-22 12:24:11 +0200

    Script to download one crawl dtype

---
+++ scripts/download_one_crawl_dtype_sbatch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=download_one_crawl_data
+#SBATCH --output=../logs/download_one_crawl_dtype_%j.output # Name of stdout output file
+#SBATCH --error=../logs/download_one_crawl_dtype_%j.erros  # Name of stderr error file
+#SBATCH --account=project_462000086
+#SBATCH --time=04:00:00
+#SBATCH --ntasks=84
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=1000
+#SBATCH --partition=small 
+
+module purge
+module load LUMI/22.12 
+module load parallel/20230322
+module load wget/1.21.3-cpeAOCC-22.12
+module load cray-python
+
+# $1 test run true or else
+# $2 crawl id
+# $3 data_type
+
+srun \
+        bash download_crawl.sh \
+        f \
+        2023-14 \
+        document
+
