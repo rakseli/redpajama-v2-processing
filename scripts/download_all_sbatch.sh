f3ea59471bf4645d1f2f0bd74885ce28a39bdf0c
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2023-12-22 14:52:51 +0200

    Set right slurm partition

--- scripts/download_all_sbatch.sh
+++ scripts/download_all_sbatch.sh
@@ -8,7 +8,7 @@
 #SBATCH --ntasks-per-node=1  # Number of tasks per node
 #SBATCH --cpus-per-task=8    # N cpus
 #SBATCH --mem-per-cpu=1000
-#SBATCH --partition=standard
+#SBATCH --partition=small
 #SBATCH --array=0-6          # Download first 7 crawls, continue 7-82, 83 has alrady been downloaded, when inodes are available use 7-82%8 to download all 8 jobs at a time
 module purge
 module load LUMI/22.12 
