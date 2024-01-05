dae41cb98dd0e493bb12bf156469cbf334690ec1
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-01-05 13:36:23 +0200

    fix wrong module

--- scripts/combine_parquet_one_crawl_sbatch.sh
+++ scripts/combine_parquet_one_crawl_sbatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=combine_parquet_minhash
-#SBATCH --output=../logs/combine_parquet_minhash_%j.output # Name of stdout output file
-#SBATCH --error=../logs/combine_parquet_minhash_%j.erros  # Name of stderr error file
+#SBATCH --job-name=combine_parquet
+#SBATCH --output=../logs/combine_parquet_%j.output # Name of stdout output file
+#SBATCH --error=../logs/combine_parquet_%j.erros  # Name of stderr error file
 #SBATCH --account=project_462000353
 #SBATCH --time=05:00:00
 #SBATCH --ntasks=1
@@ -19,4 +19,4 @@ srun \
     singularity exec \
     -B "$SING_BIND" \
     "$CONTAINER" \
-    python /scratch/project_462000353/akselir/redpajama-v2/src/combine_parquet_files.py
+    python /scratch/project_462000353/akselir/redpajama-v2/src/combine_parquet.py
