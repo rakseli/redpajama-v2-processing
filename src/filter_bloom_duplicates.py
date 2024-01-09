36e3eebd4cc2dad2f199a6cb2e0508fe2ba383f6
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-01-09 12:50:56 +0200

    Filter duplicates from both minhashes and documents

--- src/filter_bloom_duplicates.py
+++ src/filter_bloom_duplicates.py
@@ -1,8 +1,13 @@
 import datasets
 import sys
+import os
+import json
 import argparse
+import gc
+import pyarrow.parquet as pq
 from datasets import load_dataset
-from file_helpers import gather_files, custom_file_sort, check_subfolders
+from file_helpers import gather_files
+from minhashlsh import naive_data_collator
 from timer import Timer
 from tqdm import tqdm
 from pathlib import Path
@@ -19,15 +24,12 @@ datasets.logging.set_verbosity_error()
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--duplicate_path", type=str, help="single file or dir", default='/scratch/project_462000353/data/redpajama-v2/duplicates-2023-14')
-parser.add_argument("--text_path", type=str, help="single file or dir", default='/scratch/project_462000353/data/redpajama-v2/texts-2023-14')
-parser.add_argument("--lang", type=str, help="which language to filter", default='en')
-parser.add_argument("--cache_dir", type=str, help="`cache_dir` in load_dataset", default="/scratch/project_462000353/data/redpajama-v2/datasets_cache")
-parser.add_argument("--batch_size",type=int,help="batch size to use for dataset iteration",default=1000)
-parser.add_argument("--num_proc",type=str,help="number of processes for deduplication",default=1)
-parser.add_argument("--clean_cache",type=str,help="wheter to clean HF cache",default='false')
-parser.add_argument("--save",type=str,help="help wheter to save outputs",default='true')
-parser.add_argument("--output_dir",type=str,help="where to write deduplicated dataset",default="/scratch/project_462000353/data/redpajama-v2")
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", type=str, help="path to parent dir of files",default="/scratch/project_462000353/data/redpajama-v2/full_data/")
+parser.add_argument("--cache_dir", type=str, help="path HF cache dir",default="/scratch/project_462000353/data/redpajama-v2/datasets_cache")
+parser.add_argument("--crawl",type=str,help="crawl id", default='2014-15')
+parser.add_argument("--test",help="whether to test",action='store_true')
+parser.add_argument("--output_dir",type=str,help="where to write dataset",default="bloom_deduplicated")
 args = parser.parse_args()
 
 def load_text_data(path,cache_dir):
@@ -41,7 +43,6 @@ def load_text_data(path,cache_dir):
             data_files = path
     #use split parameter to obtain Dataset-object
     data = load_dataset("json",data_files=data_files,split='train',cache_dir=cache_dir,streaming=True)
-    data = data.select_columns(['raw_content'])
     return data
 
 def load_duplicate_data(path,cache_dir):
@@ -55,44 +56,75 @@ def load_duplicate_data(path,cache_dir):
             data_files = path
     #use split parameter to obtain Dataset-object
     data = load_dataset("parquet",data_files=data_files,split='train',cache_dir=cache_dir,streaming=True)
-    data = data.select_columns(['raw_content'])
+    data = data.select_columns(['id'])
     return data
+
+def load_minhash_data(path,cache_dir):
+    """Load minhash data
+
+    Args:
+        path (list,str): path to sinle file, dir or list of files 
+        cache_dir (str): HF cache dir
+
+    Returns:
+        IterableDataset: 
+    """    
+
+    if isinstance(path,list):
+        print(f"Starting loading {len(path)} files...")
+        data_files = path
+    elif isinstance(path,str):
+        if Path(path).is_dir():
+            data_files = gather_files(path)
+        else:
+            data_files = path
+    #use split parameter to obtain Dataset-object
+    data = load_dataset("parquet",data_files=data_files,split='train',cache_dir=cache_dir,streaming=True)
+    return data
+
+def create_duplicate_set(id_dataloader):
+    id_set = set()
+    for b in id_dataloader:
+        id_set.update(b['id'])
+    return id_set
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
-    text_files = gather_files(args.text_path)
-    minhash_files = gather_files(args.minhash_path)
-    sorted_by_lang = custom_file_sort(text_files,file_type='text',sort_criteria='lang')
-    if args.lang == 'de':
-        texts = sorted_by_lang[:9999]
-    elif args.lang == 'en':
-        texts = sorted_by_lang[10000:19999]
-        if args.test == 'true':
-            texts = texts[:5]
-    elif args.lang == 'es':
-        texts = sorted_by_lang[20000:29999]
-    elif args.lang == 'fr':
-        texts = sorted_by_lang[30000:39999]
-    elif args.lang == 'it':
-        texts = sorted_by_lang[40000:]
-    else:
-        raise NotImplementedError(f"Only DE, EN, ES, FR and IT available, {args.lang} given!")
-    
-    #open correspoding files that ids match
-    minhashes = [f"{args.minhash_path}/{i[59:-8]}.minhash.parquet" for i in texts]
-    text_data = load_text_data(texts,args.cache_dir)
-    id_data = load_id_data(minhashes,args.cache_dir)
-    dataloader_text = DataLoader(text_data, batch_size=args.batch_size,num_workers=args.num_proc,collate_fn=naive_data_collator)
-    dataloader_id = DataLoader(id_data, batch_size=args.batch_size,num_workers=args.num_proc,collate_fn=naive_data_collator)
-    if args.save == 'true' and args.output_dir is not None:
-        with open(f"{args.output_dir}/test_dir/{args.lang}_texts_with_ids.jsonl", 'w') as jsonl_file:
-            for text_batch,id_batch in zip(dataloader_text,dataloader_id):
-                for text_dict,id_dict in zip(text_batch,id_batch):
-                    text_dict.update(id_dict)
-                    json_line = json.dumps(text_dict,ensure_ascii=False)
-                    jsonl_file.write(json_line + '\n')
-    else:
-        for text_batch,id_batch in zip(dataloader_text,dataloader_id):
-            for t,i in zip(text_batch,id_batch):
-                print("Example sample")
-                print(t,i)
-                break
+    t = Timer()
+    if len(os.sched_getaffinity(0))<5:
+        raise Exception(f"At least 5 CPUs must be available, only {len(os.sched_getaffinity(0))} can be found!")
+    full_output = f"{args.path}/{args.crawl}/{args.output_dir}"
+    if not os.path.exists(full_output):
+        os.mkdir(full_output)
+    num_cpus=len(os.sched_getaffinity(0))
+    for lang in ["en","de","it","es","fr"]:
+        with t(f"{lang} dedup"):
+            duplicates = load_duplicate_data(f"{args.path}/{args.crawl}/combined/{lang}_duplicates.parquet",args.cache_dir)
+            dataloader_dup= DataLoader(duplicates, batch_size=10000,num_workers=num_cpus,collate_fn=naive_data_collator)
+            id_set = create_duplicate_set(dataloader_dup)
+            del duplicates
+            del dataloader_dup
+            documents = load_text_data(f"{args.path}/{args.crawl}/combined/{lang}_document.jsonl",args.cache_dir)
+            minhashes = load_minhash_data(f"{args.path}/{args.crawl}/combined/{lang}_minhash.parquet",args.cache_dir)
+            documents = documents.map(function=lambda example: example["id"] not in id_set)
+            minhashes = minhashes.map(function=lambda example: example["id"] not in id_set)
+            dataloader_doc = DataLoader(documents, batch_size=10000,num_workers=num_cpus,collate_fn=naive_data_collator)
+            dataloader_min = DataLoader(minhashes,batch_size=10000,num_workers=num_cpus,collate_fn=naive_data_collator)
+            gc.freeze()
+            gc.disable()
+            with open(f"{full_output}/{lang}_document_bloom_dedup.jsonl", 'w') as jsonl_file:
+                for batch in dataloader_doc:
+                    for json_object in batch:
+                        json_line = json.dumps(json_object,ensure_ascii=False)
+                        jsonl_file.write(json_line + '\n')
+                        
+            schema = minhashes.features.arrow_schema
+            writer = pq.ParquetWriter(f"{full_output}/{lang}_minhash_bloom_dedup.parquet", schema)
+            for table in dataloader_min:
+                writer.write_table(table=table)
+   
+            writer.close()
+
+        gc.enable()
+        gc.collect()
