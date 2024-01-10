07a84b820e361ee771e4b1214e99cc3afbe5dea5
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-01-10 16:14:12 +0200

    Script to combine downloaded files

--- scripts/combine_all_sbatch.sh
+++ scripts/combine_all_sbatch.sh
@@ -7,10 +7,13 @@
 #SBATCH --ntasks=1                                # Number of tasks                     
 #SBATCH --ntasks-per-node=1                       # Number of tasks per node
 #SBATCH --cpus-per-task=32                        # N cpus
-#SBATCH --mem-per-cpu=1200                        
+#SBATCH --mem-per-cpu=1900                        
 #SBATCH --partition=small
-#SBATCH --array=1-6                            #Process 9 crawls at a time use 7-83%9  
+#SBATCH --array=0-8                            #Process 9 crawls at a time use 7-83%9  
 module purge
+module load LUMI/22.12 
+module load parallel/20230322
+module load cray-python
 
 doc_urls="document-urls.txt"
 uniq_crawls=($(grep -o -E "[0-9]{4}-[0-9]{2}" "/scratch/project_462000353/data/redpajama-v2/urls/$doc_urls" | awk '!seen[$0]++'))
@@ -25,17 +28,29 @@ srun singularity exec \
         "$CONTAINER" \
         python /scratch/project_462000353/akselir/redpajama-v2/src/add_document_ids.py --crawl ${uniq_crawls[${SLURM_ARRAY_TASK_ID}]}
 
-#runtime ~4h
+#runtime ~5h
 srun singularity exec \
         -B "$SING_BIND" \
         "$CONTAINER" \
          python /scratch/project_462000353/akselir/redpajama-v2/src/combine_parquet.py --crawl ${uniq_crawls[${SLURM_ARRAY_TASK_ID}]}
 
+#runtime guess 6h?
 
-srun singularity exec \
-    -B "$SING_BIND" \
-    "$CONTAINER" \
-    python /scratch/project_462000353/akselir/redpajama-v2/src/combine_jsonl.py --crawl ${uniq_crawls[${SLURM_ARRAY_TASK_ID}]}
+# Parameters and options
+lang=("en" "de" "es" "fr" "it")
+data_type=("document" "quality_signals")
+
+# Generate combinations
+combinations=()
+for l in "${lang[@]}"; do
+  for d in "${data_type[@]}"; do
+    combinations+=("${uniq_crawls[${SLURM_ARRAY_TASK_ID}]} $d $l")
+  done
+done
+
+echo $combinations
+#combine all jsonl files in parallel processes where 
+srun echo "${combinations[@]}" | parallel -k -j 10 combine_jsonl.sh {}
 
 #remove sharded data
 srun rm -rf /scratch/project_462000353/data/redpajama-v2/full_data/${uniq_crawls[${SLURM_ARRAY_TASK_ID}]}/{document,document_with_ids,duplicates,minhash,quality_signals}
