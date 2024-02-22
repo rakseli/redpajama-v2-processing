f5dcd87ca0ffd5a9ae1ad1a052b9678835f299f7
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-02-22 15:46:28 +0200

    slurm script to downsample signature files

---
+++ scripts/downsample_parquet_sbatch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=fr_downsample_parquet
+#SBATCH --output=../logs/fr_downsample_parquet_%j.output # Name of stdout output file
+#SBATCH --error=../logs/fr_downsample_parquet_%j.erros  # Name of stderr error file
+#SBATCH --account=project_462000444
+#SBATCH --time=04:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4000
+#SBATCH --partition=small
+
+module purge
+# singularity setup
+CONTAINER="/scratch/project_462000353/akselir/containers/preprocessing_container.sif"
+SING_BIND="/scratch/project_462000353/"
+
+
+srun \
+    singularity exec \
+    -B "$SING_BIND" \
+    "$CONTAINER" \
+    python /scratch/project_462000353/akselir/redpajama-v2/src/downsample_parquet.py --lang fr
+
