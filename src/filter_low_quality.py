098b82096a45d016b6470a8378bdbd1fa13f9707
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-02-22 16:07:06 +0200

    Add process forking

--- src/filter_low_quality.py
+++ src/filter_low_quality.py
@@ -12,6 +12,7 @@ from filter_exact_duplicates import load_iterable_dataset
 from timer import Timer
 from torch.utils.data import DataLoader
 
+mp.set_start_method("fork", force=True)
 
 datasets.logging.set_verbosity_error()
 
@@ -20,10 +21,10 @@ parser.add_argument("--path", type=str, help="path to parent dir of files",defau
 parser.add_argument("--cache_dir", type=str, help="path HF cache dir",default="/scratch/project_462000353/data/redpajama-v2/datasets_cache")
 parser.add_argument("--crawl",type=str,help="crawl id", default='2014-15')
 parser.add_argument("--strict",action='store_true',help="wheter to use strict or loose ids")
+parser.add_argument("--extreme",action='store_true',help="wheter to use extreme filtering")
 parser.add_argument("--output_dir",type=str,help="where to write dataset",default="quality_filtered")
 parser.add_argument("--test",action='store_true')
 
-mp.set_start_method("fork", force=True)
 
 
 def create_set(id_dataloader):
@@ -41,9 +42,12 @@ if __name__ == "__main__":
         os.mkdir(full_output)
     #leave on CPU for writing the files
     num_cpus=len(os.sched_getaffinity(0))-1
-    for lang in ["en","fr","it","de","es"]:
+    #for lang in ["en","fr","it","de","es"]:
+    for lang in ["fr","de","es"]:
         if args.strict:
             id_file = f"{args.path}/{args.crawl}/quality_filtered_ids/{lang}_quality_filtered_ids_strict.jsonl.gz"
+        elif args.extreme:
+            id_file = f"{args.path}/{args.crawl}/quality_filtered_ids/{lang}_quality_filtered_ids_strictest.jsonl.gz"
         else:
             id_file = f"{args.path}/{args.crawl}/quality_filtered_ids/{lang}_quality_filtered_ids.jsonl.gz"
         gc.disable()
@@ -56,18 +60,21 @@ if __name__ == "__main__":
         print(f"Time creating id set: {format_duration(int(t.elapsed_times.get(f'{lang} id set', 0)))}")
         del ids_to_keep
         del dataloader_ids
-        
-        for d_type in ['minhash','document']:
+        for d_type in ['document','minhash']:
             if d_type == 'minhash':
                 input_file = f"{args.path}/{args.crawl}/exact_deduplicated/{lang}_{d_type}_exact_dedup.parquet"
                 if args.strict:
                     output_file = f"{full_output}/{lang}_{d_type}_quality_filtered_strict.parquet"
+                elif args.extreme:
+                    output_file = f"{full_output}/{lang}_{d_type}_quality_filtered_strictest.parquet"
                 else:
                     output_file = f"{full_output}/{lang}_{d_type}_quality_filtered.parquet"
             else:
                 input_file = f"{args.path}/{args.crawl}/exact_deduplicated/{lang}_{d_type}_exact_dedup.jsonl.gz"
                 if args.strict:
                     output_file = f"{full_output}/{lang}_{d_type}_quality_filtered_strict.jsonl"
+                elif args.extreme:
+                    output_file = f"{full_output}/{lang}_{d_type}_quality_filtered_strictest.jsonl"
                 else:
                     output_file = f"{full_output}/{lang}_{d_type}_quality_filtered.jsonl"
                 
