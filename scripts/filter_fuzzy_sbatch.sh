259936bed384ddf6bb1cc256104ec36b1bd7a7c4
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-03-12 12:59:18 +0200

    slurm script to filter fuzzy duplicates

---
+++ scripts/filter_fuzzy_sbatch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=filter_fuzzy
+#SBATCH --output=../logs/filter_fuzzy_%j.output # Name of stdout output file
+#SBATCH --error=../logs/filter_fuzzy_%j.error  # Name of stderr error file
+#SBATCH --account=project_462000444
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=6250
+#SBATCH --partition=small
+
+module purge
+
+# singularity setup
+CONTAINER="/scratch/project_462000353/akselir/containers/preprocessing_container.sif"
+SING_BIND="/scratch/project_462000353/"
+
+echo "Mem per cpu: $SLURM_MEM_PER_CPU"
+echo "CPUs per task: $SLURM_CPUS_PER_TASK"
+
+srun \
+    singularity exec \
+    -B "$SING_BIND" \
+    "$CONTAINER" \
+    python /scratch/project_462000353/akselir/redpajama-v2/src/filter_fuzzy_duplicates.py --lang $1 --lower_target
+
+
