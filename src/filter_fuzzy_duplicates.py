dd3c41d2502ddec8320a7a3edcfcaf25fa46cdbf
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-03-12 11:25:50 +0200

    Parameters for lower target files

--- src/filter_fuzzy_duplicates.py
+++ src/filter_fuzzy_duplicates.py
@@ -23,6 +23,9 @@ parser.add_argument("--cache_dir", type=str, help="path HF cache dir",default="/
 parser.add_argument("--strict",action='store_true',help="wheter to use strict or loose ids")
 parser.add_argument("--extreme",action='store_true',help="wheter to use extreme filtering")
 parser.add_argument("--output_dir",type=str,help="where to write dataset",default="final_data")
+parser.add_argument("--lang",type=str,help="lang",default="it")
+parser.add_argument("--all",help="whether to test",action='store_true')
+parser.add_argument("--lower_target",help="whether to use lower target files",action='store_true')
 
 
 
@@ -41,11 +44,17 @@ if __name__ == "__main__":
         os.mkdir(full_output)
     #leave on CPU for writing the files
     num_cpus=len(os.sched_getaffinity(0))-1
-    for lang in ["en","fr","it","de","es"]:
+    if args.all:
+        langs_iterable = ["en","fr","it","de","es"]
+    else:
+        langs_iterable = [args.lang]
+    for lang in langs_iterable:
         if args.strict:
             id_file = f"{args.id_path}/{lang}_signature_sim0.8_cross_crawl_fuzzy_dedup_ids_strict.jsonl"
         elif args.extreme:
             id_file = f"{args.id_path}/{lang}_signature_sim0.8_cross_crawl_fuzzy_dedup_ids_strictest.jsonl"
+        elif args.lower_target:
+            id_file = f"{args.id_path}/{lang}_signature_sim0.8_cross_crawl_fuzzy_dedup_ids_lower_target.jsonl"
         else:
             id_file = f"{args.id_path}/{lang}_signature_sim0.8_cross_crawl_fuzzy_dedup_ids.jsonl"
         gc.disable()
@@ -66,9 +75,12 @@ if __name__ == "__main__":
             doc_files = [x for x in all_files if f"{lang}_document_quality_filtered_strictest.jsonl" in x]
             output_file = f"{full_output}/{lang}_exact_fuzzy_quality_filtered_documents_strictest.jsonl"
         else:
-            doc_files = [x for x in all_files if f"{lang}_document_quality_filtered.jsonl" in x]
+            if args.lang=='en':
+                doc_files = [x for x in all_files if f"{lang}_document_quality_filtered_strict.jsonl" in x]
+            else:
+                doc_files = [x for x in all_files if f"{lang}_document_quality_filtered.jsonl" in x]
+                
             output_file = f"{full_output}/{lang}_exact_fuzzy_quality_filtered_documents.jsonl"
-              
         data = load_iterable_dataset(doc_files,args.cache_dir,'document')
         data = data.filter(function=lambda example: example["id"] in id_set)
         dataloader= DataLoader(data, batch_size=10000,num_workers=num_cpus,collate_fn=naive_data_collator)
@@ -82,5 +94,4 @@ if __name__ == "__main__":
         gc.enable()
         gc.collect()
         del id_set
-
        
